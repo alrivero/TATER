db4066f3dc0ff19e31d92a0407d168a7390d6744
alrivero <alfredo.rivero@outlook.com>
2025-04-13 13:57:00 +0200

    Turned va into tensor

--- datasets/iHiTOP_dataset_parallel.py
+++ datasets/iHiTOP_dataset_parallel.py
@@ -383,7 +383,7 @@ class iHiTOPDatasetParallel(BaseVideoDataset):
         video_dict = {}
         video_dict["fps"] = video_group.attrs["fps"]
         video_dict["audio_sample_rate"] = video_group.attrs["sample_rate"]
-        video_dict["valence_arousal"] = video_group["valence_arousal"][()]
+        video_dict["valence_arousal"] = torch.tensor(video_group["valence_arousal"][()])
 
         # Gather all image data (subsample every N frames within start:end)
         N = 3
