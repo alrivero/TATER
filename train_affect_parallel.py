d0370480876290f0d54541c50800200999c4b6c8
alrivero <alfredo.rivero@outlook.com>
2025-04-18 20:23:13 +0200

    affect

--- train_affect_parallel.py
+++ train_affect_parallel.py
@@ -136,7 +136,7 @@ def train(rank, world_size, config):
                     if rank == 0 and batch_idx % 1 == 0:
                         print(out['valence_arousal_out'])
                         print(out['grad_norm_total'])
-                        print(out['base_encode'].std(dim=0), out['base_encode'].mean(dim=0))
+                        print(out['base_encode'].std(dim=1), out['base_encode'].mean(dim=1))
                 except Exception as e:
                     if rank == 0:
                         print(f"Error loading batch_idx {batch_idx}!")
